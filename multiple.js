391fd89ff6f8cfc13a3a486847a92e88404ff5d0
hanxbeen <whgksqls13@naver.com>
2021-08-18 22:45:57 +0900

    study : '백준 곱셈에 대한 풀이 및 입출력 해결'

--- multiple.js
+++ multiple.js
@@ -3,10 +3,15 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 const num1 = Number(input[0]);
 const num2 = Number(input[1]);
+// num1 : 472
+// num2 : 385
 
 const oneNum = num2 % 10;
+// 일의자리 5
 const tenNum = Math.floor((num2 % 100) / 10);
+// 십의자리 8
 const hundredNum = Math.floor(num2 / 100);
+// 백의자리 3
 
 console.log(num1 * oneNum);
 console.log(num1 * tenNum);
