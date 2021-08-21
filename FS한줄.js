7fd4ae87c533abf8a6b4cfa196bdb8a07fbd1550
hanxbeen <whgksqls13@naver.com>
2021-08-21 15:50:49 +0900

    study : '1부터 원하는 수까지의 합과 곱'

--- FS한줄.js
+++ FS한줄.js
@@ -6,3 +6,8 @@ let num = Number(input);
 for (let i = 1; i <= num; i++) {
   console.log(i);
 }
+//////////////////////////////////////////////////////////////////////
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+let num = Number(input[0]);
+
+console.log('정답을 이곳에 태우세요');
